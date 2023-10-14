e78c89cea3410d7b97597e66524421ca9bbe1ce1
josuesauca <josuesauca@gmail.com>
2023-10-13 23:12:16 -0500

    nuevo

--- administrador/models.py
+++ administrador/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-
+from django.dispatch import receiver
 
 # Create your models here.
 class Tarjeta(models.Model):
@@ -21,20 +21,22 @@ class Pasajero(models.Model):
 
     def __str__(self):
         return f"Pasajero : {self.nombres}"
-    
+
+@receiver(post_save,sender=User)
 def create_profile(sender, instance,created, **kwargs):
     if created:
         Pasajero.objects.create(idUsuario=instance)
         print("Profile created")
 
-post_save.connect(create_profile, sender=User)
+#post_save.connect(create_profile, sender=User)
 
+@receiver(post_save,sender=User)
 def update_profile(sender, instance,created,**kwargs):
     if created == False:
         instance.pasajero.save()
         print('Profile updated')
 
-post_save.connect(update_profile, sender=User)
+#post_save.connect(update_profile, sender=User)
 
 class Cooperativa(models.Model):
     idCooperativa = models.AutoField(primary_key=True)
